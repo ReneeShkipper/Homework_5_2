dff27b8374aa2303201d87b5ed3b11e73a2d098d
ReneeShkipper <marinkagnome@gmail.com>
2023-11-13 17:14:05 +0300

    Add project files.

---
+++ Homework_5_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+class program
+{
+    static float Average(int [] mas)
+    {
+        int max = mas[0];
+        foreach (int i in mas)
+        {
+            max = max > i ? max : i;
+        }
+        return max;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input number");
+        int N = int.Parse(Console.ReadLine());
+        int[] mas = new int[N];
+        for (int i=0; i < N; i++)
+        {
+            mas[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine(Average(mas));
+    }
+}
